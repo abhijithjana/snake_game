3d33737b326e3a83b8ad6ba263ec1ea916168ee7
Hiro <laciferin@gmail.com>
2021-06-19 05:43:25 +0530

    js/motions.js

---
+++ js/motions.js
@@ -0,0 +1,44 @@
+const reset = () => (V = { s: V.s, x: 0, y: 0 })
+
+const willCollide = () =>
+  snake[0].x >= 18 ||
+  snake[0].x <= 0 ||
+  snake[0].y >= 18 ||
+  snake[0].y <= 0 ||
+  euthanasia()
+
+//Bugged for 2 and 1 body snake
+const euthanasia = () => {
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
+  }
+}
+
+function motionUp() {
+  reset()
+  motion.play()
+  V.x = 0
+  V.y = -1
+}
+function motionDown() {
+  reset()
+  motion.play()
+  V.x = 0
+  V.y = 1
+}
+function motionLeft() {
+  reset()
+  motion.play()
+  V.x = -1
+  V.y = 0
+}
+function motionRight() {
+  reset()
+  motion.play()
+  V.x = 1
+  V.y = 0
+}
+
+function setSpeed(num) {
+  V.s = num
+}
